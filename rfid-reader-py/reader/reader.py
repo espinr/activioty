ebcb94eb298b7488d6e9bf7246078f08b0160687
Martin Alvarez-Espinar <martin.alvarez@fundacionctic.org>
2018-01-24 13:18:59 +0100

    Library to communicate with IND903 device in Python

---
+++ rfid-reader-py/reader/reader.py
@@ -0,0 +1,28 @@
+'''
+Created on Jan 21, 2018
+
+@author: martin
+'''
+
+class Reader(object):
+    '''
+    Generic reader with basic methods to do inventories of things.
+    '''
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        pass
+        
+    def initialize(self):
+        '''
+        Initializes the reader
+        '''
+        pass
+    
+    def doInventory(self):
+        '''
+        Initializes the reader
+        '''
+        pass    
